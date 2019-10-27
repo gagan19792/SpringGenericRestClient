2f91a6564571de839e0b78392cd28104fcf25a20
gagan19792 <gagan.ubbey@gmail.com>
2019-10-26 19:31:48 -0700

    first commit

---
+++ src/main/java/com/learn/spring/rest/SpringGenericRestClientApplication.java
@@ -0,0 +1,63 @@
+package com.learn.spring.rest;
+
+import java.io.IOException;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+
+@SpringBootApplication
+public class SpringGenericRestClientApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenericRestClientApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ResponseErrorHandler responseHandler = new ResponseErrorHandler() {
+
+			@Override
+			public boolean hasError(ClientHttpResponse response) throws IOException {
+
+				System.out.println(response.getStatusText());
+				
+				if (response.getStatusCode() != HttpStatus.OK) {
+					System.out.println(response.getStatusText());
+					System.out.println(response.getStatusText());
+				}
+				return response.getStatusCode() == HttpStatus.OK ? false : true;
+			}
+
+			@Override
+			public void handleError(ClientHttpResponse response) throws IOException {
+				// TODO Auto-generated method stub
+
+			}
+		};
+
+		try {
+
+			String responseBody = new GenericRestClient<String, String>().execute(
+					new RequestDetails("https://jsonplaceholder.typicode.com/todos", HttpMethod.GET), " ", responseHandler,
+					String.class);
+
+			System.out.println(responseBody);
+
+			/*responseBody = new GenericRestClient<String, String>().execute(
+					new RequestDetails("http://localhost:8082/create", HttpMethod.POST), "Data", responseHandler,
+					String.class);
+
+			System.out.println(responseBody);*/
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
